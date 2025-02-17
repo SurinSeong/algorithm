4bfe523b0168402a8dc35f41c6a33db79871c943
surin <tjdtnfls1128@gmail.com>
2025-02-18 02:08:38 +0900

    In Class

--- APS/calculator1.py
+++ APS/calculator1.py
@@ -10,7 +10,8 @@ top = -1
 icp = {'(': 3, '*': 2, '/': 2, '+': 1, '-': 1}      # 스택 외부에서의 토큰의 우선순위
 isp = {'(': 0, '*': 2, '/': 2, '+': 1, '-': 1}      # 스택 내부에서의 토큰의 우선순위
 
-fx = '(6+5*(2-8)/2)'
+# fx = '(6+5*(2-8)/2)'
+fx = '2+3*4/5'
 
 susik = ''
 
@@ -26,11 +27,18 @@ for x in fx:
         if (top == -1) or (isp[stack[top]] < icp[x]):     # 스택에 아무것도 없거나 토큰의 우선순위가 높으면
             top += 1    # push
             stack[top] = x
+            
         elif isp[stack[top]] >= icp[x]:     # 토큰의 우선 순위가 낮으면 (else 가능)
+            
             while top > -1 and isp[stack[top]] >= icp[x]:
                 susik += stack[top]     # 우선순위가 높은 것들을 pop
                 top -= 1
+            
             top += 1
             stack[top] = x      # 그 후에 push
+
+while top > -1:
+    susik += stack[top]
+    top -= 1
 
 print(susik)
