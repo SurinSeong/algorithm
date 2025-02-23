695b4702134f6c078203722090bba76510d76338
surin <tjdtnfls1128@gmail.com>
2025-02-24 08:13:48 +0900

    Solving..

--- solving-club/4880.py
+++ solving-club/4880.py
@@ -16,21 +16,36 @@ sys.stdin = open(filename, 'r')
 - 처음 선택한 카드는 바꾸지 않음.
 - 분할 정복 문제 같아..
 """
-def game(arr, n):
-    i = 0
-    j = n-1
+def divide_and_conquer(arr, n):
+    # 카드의 개수가 하나 이하이면 리스트 그래도 반환
+    if n <= 1:
+        return arr
     
-    if j >= 2:
-        group1, group2 = arr[i:(i+j)//2+1], arr[(i+j)//2+1:j+1]
-        game(group1, len(group1))
-        game(group2, len(group2))
-        
-    else:
-        if ((arr[i] == 1) and (arr[j] == 2)) or ((arr[i] == 2) and (arr[j] == 3)) or ((arr[i] == 3) and (arr[j] == 1)):
-            return j
-        
+    # 요소의 개수가 2개 이상인 경우
+    # 두 그룹으로 나누기 위해 중산 숫자를 저장한다.
+    i, j = 0, n-1
+    middle = (i+j)//2+1
+    group1 = divide_and_conquer(arr[:middle], len(arr[:middle]))
+    group2 = divide_and_conquer(arr[middle:], len(arr[middle:]))
+    
+    # 새로 정렬한 리스트
+    result = []
+    
+    # 두 그룹을 분할 정복하기
+    # 두 그룹의 요소가 다 있는 경우
+    while group1 and group2:
+        if ((group1[0] == 1) and (group2[0] == 2)) or ((group1[0] == 2) and (group2[0] == 3)) or ((group1[0] == 3) and (group2[0] == 1)):
+            result.append(group2.pop())
         else:
-            return i
+            result.append(group1.pop())
+    
+    while group1:
+        result.append(group1.pop())
+        
+    while group2:
+        result.append(group2.pop())
+    
+    return result
 
 
 
