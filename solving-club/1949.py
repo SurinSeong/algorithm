ae0e1646e1e62a9256ac911c104b3d950905a437
surin <tjdtnfls1128@gmail.com>
2025-02-27 22:25:42 +0900

    Use DFS

--- solving-club/1949.py
+++ solving-club/1949.py
@@ -20,7 +20,7 @@ sys.stdin = open(filename, 'r', encoding='utf-8')
 - 1 <= K <= 5
 - 1 <= 지형의 높이 <= 20
 """
-# 시작 시간 : 14:15 ~
+# 시작 시간 : 14:15 ~ 18:00 20:00~20:30
 # 가장 높은 곳의 높이 찾는 함수
 def find_max_heights(arr, n):
     max_height = 0
@@ -49,26 +49,31 @@ def find_highest_idx(arr, n):
 def dfs(move, i, j, k, si, sj):
     global max_move
 
+    visited[i][j] = True   # 방문한 곳 넣는 리스트
+    
     # 갈 수 있는 곳 찾아보기
     for di, dj in delta:
         if (0 <= i+di < N) and (0 <= j+dj < N):    # 배열 안에 있다면
 
-            if i+di == si and j+dj == sj:    # 첫 위치면
+            if visited[i+di][j+dj]:    # 이미 방문했던 위치면
                 continue
 
             if mountain[i][j] > mountain[i+di][j+dj]:    # 현재 위치보다 낮다면
+                visited.append([i, j])
                 # 이동
                 dfs(move+1, i+di, j+dj, k, i, j)
                 continue
 
-
             if mountain[i+di][j+dj] - k < mountain[i][j]:    # 현재 위치에 +K를 한 것이 옮길 곳보다 크다면
-                mountain[i+di][j+dj] =
+                x = mountain[i+di][j+dj] - (mountain[i][j] - 1)
+                mountain[i+di][j+dj] -= x
+                visited.append([i, j])
                 dfs(move+1, i+di, j+dj, 0, i, j)
                 mountain[i+di][j+dj] += x
 
     # 재귀 종료 조건 : 더이상 갈곳이 없을 때
     max_move = max(max_move, move)
+    visited[i][j] = False    # 방문했던 거 다시 돌려놓기
     return
 
 
@@ -88,7 +93,9 @@ for tc in range(1, T+1):
     max_move = 1
 
     delta = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-
+    
+    visited = [[False]*N for _ in range(N)]
+    
     for i, j in highest_locations:
         dfs(1, i, j, K, i, j)
 
