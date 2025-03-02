3f404cbd470ba9f5ad29066bc49a3471b1481215
surin <tjdtnfls1128@gmail.com>
2025-03-02 12:06:43 +0900

    Use DFS

--- solving-club/2105.py
+++ solving-club/2105.py
@@ -1,8 +1,8 @@
-# import sys
-# from pathlib import Path
+import sys
+from pathlib import Path
 
-# filename = Path.cwd() / 'solving-club/input/input_2105.txt'
-# sys.stdin = open(filename, 'r', encoding='utf-8')
+filename = Path.cwd() / 'solving-club/input/input_2105.txt'
+sys.stdin = open(filename, 'r', encoding='utf-8')
 
 """
 [디저트 카페]
@@ -13,31 +13,49 @@
 - 출발한 곳으로 돌아와야 함.
 - 가장 많이 먹을 수 있는 경로 -> 디저트 수?
 """
-def find_max_dessert(i, j, d, cnt, dessert, si, sj):
-    global max_desserts, max_cnt
-    
-    visited[i][j] = True
-    kind_of_dessert.append(cafes[i][j])
-    
-    di, dj = delta[d][0], delta[d][1]
-    
-    if 0 <= i+di < N and 0 <= j+dj < N:
-        if not visited[i+di][j+dj] and (cafes[i+di][j+dj] not in kind_of_dessert):    # 방문한 적도 없고 새로운 디저트 종류라면
-            find_max_dessert(i+di, j+dj, d, cnt+1, dessert+cafes[i+di][j+dj], i, j)
-        
-        if cafes[i+di][j+dj] in kind_of_dessert:
-            if d < 4:
-                find_max_dessert(i, j, d+1, cnt, dessert, i, j)
-        
-    # elif d < 4:    # 배열 안에 없는데 delta 인덱스가 4를 넘지 않았다면
-    #     find_max_dessert(i, j, d+1, cnt, dessert, i, j)
-        
-    if si == i and sj == j:    # 시작점으로 돌아왔다면
-        if max_desserts < dessert:    # 현재까지의 총 디저트 개수보다 max가 작다면
-            max_cnt = cnt    # 현재의 디저트 종류 개수로 바꿔준다.
-            visited[i][j] = False
-    
 
+"""
+1
+4
+9 8 9 8
+4 6 9 4
+8 7 7 8
+4 5 3 5
+"""
+
+def dfs(i, j, d, arr, fi, fj):
+    global max_cnt
+    
+    if d == 4:    # 델타 인덱스가 끝났으면
+        return
+    
+    # 재귀 종료 조건 1 : 만약 2차원 배열을 넘어갔다면
+    if not((0 <= i < N) and (0 <= j < N)):    
+        return
+    
+    # 재귀 종료 조건 2 : 방문 기록이 있다면
+    if visited[i][j]:
+        if i == fi and j == fj:    # 그런데 첫 번째 위치라면 (재귀 종료와 결과 도출 조건 : 처음 위치로 돌아왔다면)
+            max_cnt = max(max_cnt, len(arr))    # 여기까지의 길이 비교하기
+            return
+        else:    # 첫 번째 위치 아니면
+            return    # 그냥 리턴하기
+    
+    # 재귀 종료 조건 3 : 칸의 수가 이미 존재한다면
+    if cafes[i][j] in arr:
+        return
+    
+    # 델타의 순서는 계속 0 > 1 > 2 > 3으로 진행
+    # 이동 가능한 경우 : 델타 인덱스 0에서 다시 탐색 시작
+    arr.append(cafes[i][j])    # 숫자 리스트에 추가
+    visited[i][j] = True    # 방문 완료
+    dfs(i+delta[d][0], j+delta[d][1], d, desserts, fi, fj)
+    
+    # 이동 불가능한 경우
+    visited[i][j] = False
+    arr.pop()
+    dfs(i, j, d+1, desserts, fi, fj)
+    
 
 # 테스트 케이스 개수
 T = int(input())
@@ -49,17 +67,22 @@ for tc in range(1, T+1):
     # 대각선으로 갈 델타
     delta = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
     
-    max_desserts, max_cnt = 0, 0
-    
+    # 방문 여부 확인 배열
     visited = [[False]*N for _ in range(N)]
-    kind_of_dessert = []
+    
+    # 디저트 종류별로 넣는 곳
+    desserts = []
+    
+    # 디저트 최대 종류
+    max_cnt = 0
     
     # 시작점 잡아주기
     for i in range(N-1):
         for j in range(1, N):
-            find_max_dessert(i, j, 0, 1, cafes[i][j], i, j)
+            dfs(i, j, 0, desserts, i, j)
     
-    if max_cnt == 0:
+    # 돌 수 있는 카페가 없었다면
+    if max_cnt < 4:
         answer = -1
     else:        
         answer = max_cnt
