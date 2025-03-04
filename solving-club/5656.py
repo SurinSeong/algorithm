c961d2a01dd1529ba3c3da0e7e23a5f41fd7b186
SurinSeong <tjdtnfls1128@gmail.com>
2025-03-04 16:30:31 +0900

    To be continue..

--- solving-club/5656.py
+++ solving-club/5656.py
@@ -16,12 +16,36 @@ sys.stdin = open(filename, 'r', encoding='utf-8')
 
 남은 벽돌의 개수는??
 """
-def find_first_number_in_col(arr):
-    for j in range(W):
-        for i in range(H):
-            if blocks[i][j] > 0:
-                arr.append((i, j))
-                break
+def dfs(c):
+    if len(start_cols) == N:
+        for sj in start_cols:
+            # 1. 시작 행 잡기
+            for si in range(H):
+                if blocks[si][sj] >= 1:
+                    break
+            # 2. 벽돌 부수기
+            crush_blocks(si, sj)
+            # 3. 연속해서 다 부셨으면 정리해주기
+            move_down()
+
+        # 4. 쏘는 횟수만큼 다 쐈으면 남은 칸 수 확인하기
+        find_min()
+        return
+
+    if visited[c] == N:
+        return
+
+    start_cols.append(c)
+    visited[c] += 1
+    dfs(c)
+
+    if visited[c] == N:
+        start_cols.pop()
+        visited[c] -= 1
+        dfs(c+1)
+    else:
+        dfs(c)
+
 
 
 # 벽돌 깨기
@@ -29,20 +53,33 @@ def crush_blocks(i, j):
     
     if blocks[i][j] == 1:
         blocks[i][j] = 0
-        return
+        # return
     
     for d in range(4):
         for k in range(1, blocks[i][j]):
             ni, nj = i+delta[d][0]*k, j+delta[d][1]*k
             if (0 <= ni < W) and (0 <= nj < H):    # 범위에 있으면
                 if blocks[ni][nj] > 2:    # 1보다 큰 수가 있으면 또 이동할 수 있기 때문에
-                    crush_blocks(ni, nj)
+                    crush_blocks(ni, nj)    # 이동해서 벽돌 깨기
                 elif blocks[ni][nj] == 1:
                     blocks[ni][nj] = 0    # 0으로 바꿔주기
 
 
+# 정리
+def move_down():
+    for j in range(W):
+        new_numbers = []  # 1 이상의 숫자 넣을 리스트
+        for i in range(H-1, -1, -1):
+            if blocks[i][j] >= 1:
+                new_numbers.append(blocks[i][j])
+
+        for i in range(len(new_numbers)):
+            blocks[H-1-i][j] = new_numbers[i]
+
+
+
 # 남아있는 벽돌 수 찾기
-def find_max():
+def find_min():
     global min_cnt
     
     cnt = 0
@@ -66,10 +103,15 @@ for tc in range(1, T+1):
     
     # 상하좌우 델타
     delta = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    
+
+    visited = [0]*W
+    start_cols = []    # 횟수 만큼의 열을 담을 리스트
+
     answer = 0
     
     min_cnt = W*H
+
+    for r in range()
     
     
         
