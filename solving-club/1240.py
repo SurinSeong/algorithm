f0ab10f5d9b4d05e6d9d0c630fe0e3c83c7c3e3a
SurinSeong <tjdtnfls1128@gmail.com>
2025-03-06 14:53:42 +0900

    Update code

--- solving-club/1240.py
+++ solving-club/1240.py
@@ -13,32 +13,17 @@ import sys
 sys.stdin = open('./input/input_1240.txt', 'r')
 
 code_dic = {
-               (3, 2, 1, 1) : 0,
-               (2, 2, 2, 1) : 1,
-               (2, 1, 2, 2) : 2,
-               (1, 4, 1, 1) : 3,
-               (1, 1, 3, 2) : 4,
-               (1, 2, 3, 1) : 5,
-               (1, 1, 1, 4) : 6,
-               (1, 3, 1, 2) : 7,
-               (1, 2, 1, 3) : 8,
-               (3, 1, 1, 2) : 9
-            }
-
-# 이진을 십진으로
-def code_to_dec(code):
-    orders = [1]*4
-    idx = 0
-    first = code[0]
-    for c in code[1:]:
-        if c == first:
-            orders[idx] += 1
-        else:
-            first = c
-            idx += 1
-
-    return code_dic[tuple(orders)]
-
+   '0001101' : 0,
+   '0011001' : 1,
+   '0010011' : 2,
+   '0111101' : 3,
+   '0100011' : 4,
+   '0110001' : 5,
+   '0101111' : 6,
+   '0111011' : 7,
+   '0110111' : 8,
+   '0001011' : 9
+}
 
 # 암호 검사
 def check_password(arr):
@@ -65,23 +50,24 @@ for tc in range(1, T+1):
     # 배열 받기
     password = [list(input()) for _ in range(N)]
 
+    # 일의 자리로 변환한 암호 넣는 리스트
+    p_list = []
+
     # 암호의 시작 행을 찾기
     for i in range(N):
         if '1' in password[i]:
-            start_row = i
-            break
+            # 시작 행 뒤에서 1을 찾기
+            for j, n in enumerate(password[i][::-1], 1):
+                if n == '1':
+                    last_col = M - j + 1
+                    start_col = last_col - 56
 
-    # 시작 행 뒤에서 1을 찾기
-    for j, n in enumerate(password[start_row][::-1], 1):
-        if n == '1':
-            last_col = M-j+1
-            start_col = last_col - 56
-            break
+                    for idx in range(start_col, last_col, 7):    # 비트 쪼개서 확인하기
+                        binary_number = ''.join(password[i][idx:idx + 7])
+                        p_list.append(code_dic[binary_number])
 
-    p_list = []
-    for idx in range(start_col, last_col, 7):
-        binary_number = password[start_row][idx:idx+7]
-        p_list.append(code_to_dec(binary_number))
+                    break    # 변환 암호 다 넣고 끝내기
+            break
 
     # 암호 검사
     answer = check_password(p_list)
