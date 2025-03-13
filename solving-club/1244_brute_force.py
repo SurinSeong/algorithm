ec10d2f8a3cb8f06ce59c34da20c89910b28e32d
SurinSeong <tjdtnfls1128@gmail.com>
2025-03-13 12:33:35 +0900

    solve with GPT

--- solving-club/1244_brute_force.py
+++ solving-club/1244_brute_force.py
@@ -4,14 +4,23 @@
 - 반드시 횟수만큼 교환 (최대 10번)이 이루어져야함.
 - 오른쪽 끝에서부터 1원 -> 왼쪽으로 갈수록 10의 배수만큼 커진다.
 """
-def get_comb(cnt, comb):
-    if cnt == 2:
-        print(comb)
+def make_max_money(cnt):
+    global answer
+
+    state = (int(''.join(number_list)), cnt)    # 현재 교환 상태 체크를 위해 cnt와 함께 visited에 넣어준다.
+    if state in visited:
+        return
+    visited.add(state)
+
+    if cnt == rotation:
+        answer = max(answer, int(''.join(number_list)))
         return
 
-    for i in range(N):
-        get_comb(cnt+1, comb+[number_list[i]])
-
+    for i in range(N-1):
+        for j in range(i+1, N):
+            number_list[i], number_list[j] = number_list[j], number_list[i]
+            make_max_money(cnt+1)
+            number_list[i], number_list[j] = number_list[j], number_list[i]
 
 
 # 테스트 케이스 개수
@@ -20,43 +29,14 @@ T = int(input())
 for tc in range(1, T+1):
     # 숫자판, 교환횟수
     numbers, rotation = map(int, input().split())
-    number_list = [int(n) for n in str(numbers)]
+    number_list = [n for n in str(numbers)]
 
     N = len(number_list)    # 숫자판 길이
 
     # 최종 상금
     answer = 0
+    visited = set()
 
-    # 교환 가능한 경우의 수 찾기
-    # 1. 가장 큰 수 찾기
-    max_num = max(number_list)
-    max_idx_list = []    # 가장 큰수의 인덱스 넣는 곳
-
-    for i in range(1, len(number_list)):
-        if max_num == number_list[i]:
-            max_idx_list.append(i)
-
-    not_max = 0
-
-    # 인덱스가 0인 자리에 가장 큰 수가 들어가는 것이 맞음.
-    while rotation:
-
-        if number_list[not_max] == max_num:    # 가장 큰수이면
-            not_max += 1
-
-
-        if not_max == 0:
-            number_list[-1], number_list[-2] = number_list[-2], number_list[-1]
-            rotation -= 1
-            continue
-
-        not_max -= 1
-        # 가장 마지막 인덱스
-        mi = max_idx_list.pop()
-        if mi > not_max:
-            number_list[not_max], number_list[mi] = number_list[mi], number_list[not_max]
-            rotation -= 1
-
-    answer = ''.join(map(str, number_list))
+    make_max_money(0)
 
     print(f'#{tc} {answer}')
--- solving-club/1861.py
+++ solving-club/1861.py
@@ -7,19 +7,22 @@
 - 방이 있어야 이동 가능하고, 이동하려는 방의 숫자가 현재 방 숫자보다 정확히 1 커야 한다.
 - 처음에 어디에 있어야 가장 많은 개수의 방을 이동할 수 있을까?
 """
-def dfs(si, sj, cnt):
+def move_room(si, sj):
+    if visited[si][sj] != 0:    # 최대 이동 거리 계산 값이 있다면
+        return visited[si][sj]    # 사용한다.
+
+    max_move = 1
+
     for d in range(4):
         ni, nj = si + dy[d], sj + dx[d]
-        if 0 <= ni < N and 0 <= nj < N:    # 범위 안에 있으면
-            if not visited[ni][nj]:    # 방문 전이면
-                if rooms[ni][nj] == rooms[si][sj] + 1:    # 현재보다 하나 크다면
-                    visited[ni][nj] = True
-                    dfs(ni, nj, cnt+1)
 
+        if 0 <= ni < N and 0 <= nj < N:    # 범위 안에 있다면
+            if rooms[ni][nj] == rooms[si][sj] + 1:    # 이동 가능 조건이면
+                max_move = max(max_move, 1 + move_room(ni, nj))    # 계속 이동하면서 최대값을 구하려고 한다.
 
+    visited[si][sj] = max_move    # 최종적으로 구해진 최댓값을 방문 기록용 리스트에 저장한다.
 
-
-
+    return max_move
 
 # 테스트 케이스 개수
 T = int(input())
@@ -28,17 +31,24 @@ for tc in range(1, T+1):
     # 방의 크기
     N = int(input())
     rooms = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0]*N for _ in range(N)]    # 최대 이동 거리 저장을 위한
 
     dy = [0, 1, 0, -1]
     dx = [1, 0, -1, 0]
 
     max_cnt = 1
-    room_number = 1000000
-    
-    visited = [[False]*N for _ in range(N)]
+    room_number = N**3
 
     for i in range(N):
         for j in range(N):
-            dfs(i, j)
+            current_cnt = move_room(i, j)
+
+            move_room(i, j)
+
+            if max_cnt < current_cnt:
+                max_cnt = current_cnt
+                room_number = rooms[i][j]
+            elif max_cnt == current_cnt:
+                room_number = min(room_number, rooms[i][j])
 
     print(f'#{tc} {room_number} {max_cnt}')
